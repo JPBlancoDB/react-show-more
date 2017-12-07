0f5900343cf0e89993d202cdc22accbfd65c6d2f
JPBlancoDB <jpblancodb@gmail.com>
2017-12-07 12:33:07 -0300

    Added prop-types as dependency

--- src/ShowMore.js
+++ src/ShowMore.js
@@ -1,74 +1,77 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Truncate from 'react-truncate';
 
 class ShowMore extends Component {
-    static defaultProps = {
-        lines: 3,
-        more: 'Show more',
-        less: 'Show less',
-        anchorClass: ''
-    }
+	static defaultProps = {
+		lines: 3,
+		more: 'Show more',
+		less: 'Show less',
+		anchorClass: ''
+	};
 
-    static propTypes = {
-        children: PropTypes.node,
-        lines: PropTypes.number,
-        more: PropTypes.node,
-        less: PropTypes.node,
-        anchorClass: PropTypes.string
-    }
+	static propTypes = {
+		children: PropTypes.node,
+		lines: PropTypes.number,
+		more: PropTypes.node,
+		less: PropTypes.node,
+		anchorClass: PropTypes.string
+	};
 
-    state = {
-        expanded: false,
-        truncated: false
-    }
+	state = {
+		expanded: false,
+		truncated: false
+	};
 
-    handleTruncate = truncated => {
-        if (truncated !== this.state.truncated) {
-            this.setState({
-                truncated
-            });
-        }
-    }
+	handleTruncate = truncated => {
+		if (truncated !== this.state.truncated) {
+			this.setState({
+				truncated
+			});
+		}
+	};
 
-    toggleLines = event => {
-        event.preventDefault();
+	toggleLines = event => {
+		event.preventDefault();
 
-        this.setState({
-            expanded: !this.state.expanded
-        });
-    }
+		this.setState({
+			expanded: !this.state.expanded
+		});
+	};
 
-    render() {
-        const {
-            children,
-            more,
-            less,
-            lines,
-            anchorClass
-        } = this.props;
+	render() {
+		const { children, more, less, lines, anchorClass } = this.props;
 
-        const {
-            expanded,
-            truncated
-        } = this.state;
+		const { expanded, truncated } = this.state;
 
-        return (
-            <div>
-                <Truncate
-                    lines={!expanded && lines}
-                    ellipsis={(
-                        <span>... <a href='#' className={anchorClass} onClick={this.toggleLines}>{more}</a></span>
-                    )}
-                    onTruncate={this.handleTruncate}
-                >
-                    {children}
-                </Truncate>
-                {!truncated && expanded && (
-                    <span> <a href='#' className={anchorClass} onClick={this.toggleLines}>{less}</a></span>
-                )}
-            </div>
-        );
-    }
+		return (
+			<div>
+				<Truncate
+					lines={!expanded && lines}
+					ellipsis={
+						<span>
+							...{' '}
+							<a href="#" className={anchorClass} onClick={this.toggleLines}>
+								{more}
+							</a>
+						</span>
+					}
+					onTruncate={this.handleTruncate}
+				>
+					{children}
+				</Truncate>
+				{!truncated &&
+					expanded && (
+						<span>
+							{' '}
+							<a href="#" className={anchorClass} onClick={this.toggleLines}>
+								{less}
+							</a>
+						</span>
+					)}
+			</div>
+		);
+	}
 }
 
 export default ShowMore;
